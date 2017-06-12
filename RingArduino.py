620a331f5e242c62cbf0082b2d41fa56beba90d0
arizzi91 <a.rizzi0108@gmail.com>
2017-06-12 22:01:08 +0200

    Create RingArduino.py
    bug fix

--- RingArduino.py
+++ RingArduino.py
@@ -8,20 +8,21 @@ path = '/var/www/html/IoTProject/'
 http_path = 'http://192.168.1.69/IoTProject/'
 
 if __name__ == '__main__':
-	while True:
-		if (arduinoSerialData.readline() is not None):
-			bright = arduinoSerialData.readline()
-			brightfloat = float(bright)
-			bright_norm= int(round((1-(brightfloat/1023))*100))
-			ts = time.time()
-			im_name = 'screen' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S') + '.jpg'
-			packet = http_path + im_name
-			os.system("python3 Publisher.py %s" % packet)
-			com = 'sudo fswebcam -r 640x480 -S 15 -s brightness=' +str(bright_norm)+'%  --shadow screen.jpg ' + path + im_name
-			os.system(com)
-			pth = "\'" + packet + "\'"
-			db = MySQLdb.connect(host = "localhost", user = "root", passwd = "raspberry", db = "iot_database")
-			cur = db.cursor()
-			cur.execute("insert into Images (PATH) values(" + pth + ")")
-			db.commit()
-			db.close()
+        while True:
+                if (arduinoSerialData.readline() is not None):
+                        ts = time.time()
+                        im_name = 'screen' + datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S') + '.jpg'
+                        packet = http_path + im_name
+                        os.system("python3 Publisher.py %s" % packet)
+                        bright = arduinoSerialData.readline()
+                        bright_f = float(bright)
+                        bright_norm = int(round((1-(bright_f/1023))*100))
+                        com = 'sudo fswebcam -r 640x480 -S 15 -s brightness=' +str(bright_norm)+'%  --shadow screen.jpg ' + path + im_name
+                        os.system(com)
+                        pth = "\'" + packet + "\'"
+                        db = MySQLdb.connect(host = "localhost", user = "root", passwd = "raspberry", db = "iot_database")
+                        cur = db.cursor()
+                        cur.execute("insert into Images (PATH) values(" + pth + ")")
+                        db.commit()
+                        db.close()
+
